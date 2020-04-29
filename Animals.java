5fda81d7526e6b8649c1f9a90445f625d87a2c72
Karyna <karyna.karmaza@gmail.com>
2020-04-29 21:29:20 +0300

    Добавление родительского класса

---
+++ Animals.java
@@ -0,0 +1,20 @@
+package Lesson2;
+
+public class Animals {
+     String name, type, color;
+
+    public  void eat ()
+    {
+        System.out.println("Есть");
+    }
+
+    public void sleep ()
+    {
+        System.out.println("Спать");
+    }
+
+    public void voice()
+    {
+        System.out.println("Издавать звук");
+    }
+}
