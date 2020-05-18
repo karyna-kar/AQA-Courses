bf943df3f257775343fe87fe551818e0e186caf1
Karyna <karyna.karmaza@gmail.com>
2020-05-18 20:55:29 +0300

    Interface IProduct.java

---
+++ Task/IProduct.java
@@ -0,0 +1,9 @@
+package Task;
+
+public interface IProduct {
+    void installFirstPart(IProductPart productPart);
+
+    void installSecondPart(IProductPart productPart);
+
+    void installThirdPart(IProductPart productPart);
+}
