b444924804f91f5753648db0c64c7d377bafd911
Karina <karyna.karmaza@gmail.com>
2020-05-18 20:28:11 +0300

    Lates changes - Transliteration task

--- Task/Main.java
+++ Task/Main.java
@@ -4,13 +4,13 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 import static Task.Transliteration.getTransliteration;
-import static Task.Transliteration.parsInputs;
+import static Task.Transliteration.parsInput;
 import static Task.Transliteration.printResult;
 
 public class Main {
     public static void main(String[] args)  throws Exception {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        String example = reader.readLine();
-        printResult(getTransliteration(parsInputs(example)));
+        String inputString = reader.readLine();
+        printResult(getTransliteration(parsInput(inputString)));
     }
 }
--- Task/Transliteration.java
+++ Task/Transliteration.java
@@ -2,77 +2,95 @@ package Task;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class Transliteration {
-    public static Map<String, String> setAlphabet(){
-        Map <String, String> alphabet = new HashMap<String, String>();
-        alphabet.put("а", "a");
-        alphabet.put("б", "b");
-        alphabet.put("в", "v");
-        alphabet.put("г", "g");
-        alphabet.put("д", "d");
-        alphabet.put("е", "e");
-        alphabet.put("ё", "e");
-        alphabet.put("ж", "zh");
-        alphabet.put("з", "z");
-        alphabet.put("и", "i");
-        alphabet.put("й", "i");
-        alphabet.put("к", "k");
-        alphabet.put("л", "l");
-        alphabet.put("м", "m");
-        alphabet.put("н", "n");
-        alphabet.put("о", "o");
-        alphabet.put("п", "p");
-        alphabet.put("р", "r");
-        alphabet.put("с", "s");
-        alphabet.put("т", "t");
-        alphabet.put("у", "u");
-        alphabet.put("ф", "f");
-        alphabet.put("х", "h");
-        alphabet.put("ц", "c");
-        alphabet.put("ч", "ch");
-        alphabet.put("ш", "sh");
-        alphabet.put("щ", "sch");
-        alphabet.put("ъ", "'");
-        alphabet.put("ы", "y");
-        alphabet.put("ъ", "'");
-        alphabet.put("э", "e");
-        alphabet.put("ю", "yu");
-        alphabet.put("я", "ya");
-        return alphabet;
+    public static  Map <Character, String> alphabet = new HashMap<Character, String>();
+
+    static {
+        alphabet.put('а', "a");
+        alphabet.put('б', "b");
+        alphabet.put('в', "v");
+        alphabet.put('г', "g");
+        alphabet.put('д', "d");
+        alphabet.put('е', "e");
+        alphabet.put('ё', "e");
+        alphabet.put('ж', "zh");
+        alphabet.put('з', "z");
+        alphabet.put('и', "i");
+        alphabet.put('й', "i");
+        alphabet.put('к', "k");
+        alphabet.put('л', "l");
+        alphabet.put('м', "m");
+        alphabet.put('н', "n");
+        alphabet.put('о', "o");
+        alphabet.put('п', "p");
+        alphabet.put('р', "r");
+        alphabet.put('с', "s");
+        alphabet.put('т', "t");
+        alphabet.put('у', "u");
+        alphabet.put('ф', "f");
+        alphabet.put('х', "h");
+        alphabet.put('ц', "c");
+        alphabet.put('ч', "ch");
+        alphabet.put('ш', "sh");
+        alphabet.put('щ', "sch");
+        alphabet.put('ъ', "'");
+        alphabet.put('ы', "y");
+        alphabet.put('ъ', "'");
+        alphabet.put('э', "e");
+        alphabet.put('ю', "yu");
+        alphabet.put('я', "ya");
     }
-    public static String [] parsInputs(String example)
+    //метод парсинга строки на слова
+    public static String [] parsInput(String inputString)
     {
-        String[] words = example.split(" ");
+        String[] words = inputString.split(" ");
         return words;
     }
-
-    public static String [] getTransliteration(String [] example)
+    //главный метод по переводу вводных данных
+    public static List<String> getTransliteration(String [] words)
     {
-        Map<String, String> alphabet = setAlphabet();
-        String [] results = new String[example.length];
-        for (int j=0; j < results.length; j++) {
-            StringBuilder result = new StringBuilder();
-            for (int i = 0; i < example[j].length(); i++) {
-                String letter = example[j].substring(i, i + 1);
-                if (letter.matches("[А-Я]")) {
-                    letter = letter.toLowerCase();
-                    result = result.append((alphabet.get(letter)).toUpperCase());
-                } else {
-                    result = result.append(alphabet.get(letter));
-                }
-            }
-            results[j]=result.toString();
+        List<String> outputString = new ArrayList<String>();
+        for (int i=0; i < words.length; i++) {
+            outputString.add(translateWord(words[i]));
         }
-        return results;
+        return outputString;
     }
 
-    public static void printResult(String [] results)
+    //метод по переводу слова
+    public static String translateWord(String word)
     {
-        for (int i=0; i<results.length; i++)
+        StringBuilder outputWord = new StringBuilder();
+        char[] letters  = word. toCharArray();
+        for (int i = 0; i < letters.length; i++) {
+            outputWord = outputWord.append(translateLetter(letters[i]));
+        }
+        return outputWord.toString();
+    }
+
+    //метод по переводу буквы
+    public static String translateLetter(Character letter)
+    {
+        String translatedLetter;
+        //проверка регистра буквы
+        if (Character.toString(letter).matches("[А-Я]")) {
+            letter = Character.toLowerCase(letter);
+            translatedLetter = alphabet.get(letter).toUpperCase();
+        }
+        else {
+            translatedLetter = alphabet.get(letter);
+        }
+        return translatedLetter;
+    }
+
+    //вывод результата
+    public static void printResult( List<String> outputString)
+    {
+        for (int i=0; i<outputString.size(); i++)
         {
-            System.out.print(results[i]+" ");
+            System.out.print(outputString.get(i)+" ");
         }
     }
 }
